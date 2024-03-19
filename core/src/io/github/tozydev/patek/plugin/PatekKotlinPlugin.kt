ac31ac30735a20960c3569056cb478c28430c527
Tozy Fullbuster <tozyfullbuster@gmail.com>
2024-03-19 17:54:10 +0700

    refactor: include mccoroutine code

--- core/src/io/github/tozydev/patek/plugin/PatekKotlinPlugin.kt
+++ core/src/io/github/tozydev/patek/plugin/PatekKotlinPlugin.kt
@@ -1,6 +1,7 @@
 package io.github.tozydev.patek.plugin
 
-import com.github.shynixn.mccoroutine.bukkit.SuspendingJavaPlugin
+import com.github.shynixn.mccoroutine.bukkit.MCCoroutine
+import kotlinx.coroutines.runBlocking
 import org.bukkit.plugin.java.JavaPlugin
 import java.nio.file.Path
 
@@ -8,23 +9,45 @@ import java.nio.file.Path
  * Base class for Patek Kotlin plugins.
  * This class provides the functionality for a Paper plugin with kotlinx-coroutines support.
  */
-abstract class PatekKotlinPlugin : SuspendingJavaPlugin(), PatekPlugin {
+abstract class PatekKotlinPlugin : JavaPlugin(), PatekPlugin {
 
     override val dataDirectory: Path = dataFolder.toPath()
-
-    override suspend fun onDisableAsync() = Unit
 
     override suspend fun onLoadAsync() = Unit
 
     override suspend fun onEnableAsync() = Unit
 
-    final override fun onDisable() = super.onEnable()
+    override suspend fun onDisableAsync() = Unit
 
-    final override fun onLoad() = super.onLoad()
+    final override fun onLoad() {
+        runBlocking {
+            onLoadAsync()
+        }
+    }
 
-    final override fun onEnable() = super.onEnable()
+    final override fun onEnable() {
+        mcCoroutine.getCoroutineSession(this).isManipulatedServerHeartBeatEnabled = true
+        runBlocking {
+            onLoadAsync()
+        }
+        mcCoroutine.getCoroutineSession(this).isManipulatedServerHeartBeatEnabled = false
+    }
+
+    final override fun onDisable() {
+        runBlocking {
+            onDisableAsync()
+        }
+    }
 
     companion object {
+
+        internal val mcCoroutine: MCCoroutine by lazy {
+            try {
+                Class.forName(MCCoroutine.Driver).getDeclaredConstructor().newInstance() as MCCoroutine
+            } catch (e: Exception) {
+                throw RuntimeException("Failed to load MCCoroutine implementation. Shade patek-core into your plugin.")
+            }
+        }
 
         /**
          * Retrieves the instance of a specified plugin class.
