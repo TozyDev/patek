94ac27e94c741687202a8fec75598366cd51b006
Nguyễn Thanh Tân <114842493+TozyDev@users.noreply.github.com>
2024-03-20 12:12:20 +0700

    feat: retrive plugin extension (#6)

--- core/src/io/github/tozydev/patek/plugin/PatekKotlinPlugin.kt
+++ core/src/io/github/tozydev/patek/plugin/PatekKotlinPlugin.kt
@@ -4,9 +4,11 @@ import com.github.shynixn.mccoroutine.bukkit.MCCoroutine
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
 import dev.jorel.commandapi.CommandAPILogger
+import io.papermc.paper.plugin.provider.classloader.ConfiguredPluginClassLoader
 import kotlinx.coroutines.runBlocking
 import org.bukkit.plugin.java.JavaPlugin
 import java.nio.file.Path
+import kotlin.reflect.KClass
 
 /**
  * Base class for Patek Kotlin plugins.
@@ -39,7 +41,7 @@ abstract class PatekKotlinPlugin : JavaPlugin(), PatekPlugin {
         mcCoroutine.getCoroutineSession(this).isManipulatedServerHeartBeatEnabled = true
         runBlocking {
             CommandAPI.onEnable()
-            onLoadAsync()
+            onEnableAsync()
         }
         mcCoroutine.getCoroutineSession(this).isManipulatedServerHeartBeatEnabled = false
     }
@@ -61,14 +63,35 @@ abstract class PatekKotlinPlugin : JavaPlugin(), PatekPlugin {
                 throw RuntimeException("Failed to load MCCoroutine implementation. Shade patek-core into your plugin.")
             }
         }
-
-        /**
-         * Retrieves the instance of a specified plugin class.
-         *
-         * @param T The plugin class extending [JavaPlugin].
-         * @see JavaPlugin.getPlugin
-         */
-        @Suppress("unused")
-        inline fun <reified T : JavaPlugin> getPlugin() = getPlugin(T::class.java)
     }
 }
+
+@PublishedApi
+internal val pluginInstances by lazy { mutableMapOf<KClass<*>, PatekKotlinPlugin>() }
+
+/**
+ * Retrieves the instance of a specified plugin class.
+ *
+ * @param T The plugin class extending [PatekKotlinPlugin].
+ * @see JavaPlugin.getPlugin
+ */
+@Suppress("unused")
+inline fun <reified T : PatekKotlinPlugin> getPlugin() = pluginInstances.getOrPut(T::class) {
+    JavaPlugin.getPlugin(T::class.java)
+}
+
+private val stackWalker by lazy { StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE) }
+
+/**
+ * Retrieves the currently working plugin that calls this method.
+ *
+ * This method use [StackWalker] to retrieve the first caller class that it's class loader is a [ConfiguredPluginClassLoader].
+ */
+@Suppress("UnstableApiUsage")
+internal fun retrieveCallingPlugin() = stackWalker
+    .walk { stream ->
+        stream
+            .filter { it.declaringClass.classLoader is ConfiguredPluginClassLoader }
+            .findFirst()
+            .get()
+    }.let { JavaPlugin.getProvidingPlugin(it.declaringClass) }
