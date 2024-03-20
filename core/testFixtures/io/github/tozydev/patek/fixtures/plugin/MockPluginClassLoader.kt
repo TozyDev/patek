c1712666a3c79eb27b143207c4456ce0d26f5bf5
Nguyễn Thanh Tân <114842493+TozyDev@users.noreply.github.com>
2024-03-20 10:24:19 +0700

    build: configure unit testing (#7)
    * build: configure test mockking
    
    * test(fixtures): add plugin mocking function

---
+++ core/testFixtures/io/github/tozydev/patek/fixtures/plugin/MockPluginClassLoader.kt
@@ -0,0 +1,27 @@
+package io.github.tozydev.patek.fixtures.plugin
+
+import io.papermc.paper.plugin.configuration.PluginMeta
+import io.papermc.paper.plugin.provider.classloader.ConfiguredPluginClassLoader
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("UnstableApiUsage")
+class MockPluginClassLoader : ClassLoader(), ConfiguredPluginClassLoader {
+
+    private var holder: JavaPlugin? = null
+
+    override fun close() = Unit
+
+    override fun getConfiguration(): PluginMeta? = holder?.pluginMeta
+
+    override fun loadClass(name: String, resolve: Boolean, checkGlobal: Boolean, checkLibraries: Boolean): Class<*> =
+        super.loadClass(name, resolve)
+
+
+    override fun init(plugin: JavaPlugin?) {
+        holder = plugin
+    }
+
+    override fun getPlugin(): JavaPlugin? = holder
+
+    override fun getGroup() = throw NotImplementedError()
+}
---
+++ core/testFixtures/io/github/tozydev/patek/fixtures/plugin/MockkPlugin.kt
@@ -0,0 +1,18 @@
+package io.github.tozydev.patek.fixtures.plugin
+
+import io.github.tozydev.patek.plugin.PatekKotlinPlugin
+import io.mockk.mockkClass
+import kotlin.reflect.KClass
+
+@Suppress("UNCHECKED_CAST")
+fun mockkPlugin(
+    name: String? = null,
+    relaxed: Boolean = false,
+    vararg moreInterfaces: KClass<*>,
+    relaxUnitFun: Boolean = false,
+    block: PatekKotlinPlugin.() -> Unit = {},
+): PatekKotlinPlugin {
+    val classLoader = MockPluginClassLoader()
+    val pluginClass = classLoader.loadClass(PatekKotlinPlugin::class.qualifiedName) as Class<PatekKotlinPlugin>
+    return mockkClass(pluginClass.kotlin, name, relaxed, *moreInterfaces, relaxUnitFun = relaxUnitFun, block = block)
+}
