c767055c43708a5d843320d15b9a1f7b67c7e622
Tozy Fullbuster <tozyfullbuster@gmail.com>
2024-03-21 21:25:28 +0700

    feat(text): add orEmpty extension for nullable Components

--- core/src/io/github/tozydev/patek/text/Components.kt
+++ core/src/io/github/tozydev/patek/text/Components.kt
@@ -1,13 +1,23 @@
 package io.github.tozydev.patek.text
 
 import io.github.tozydev.patek.text.minimessage.miniMessage
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 
 /**
  * Deserializes the given string using _MiniMessage_ format with optional [tagResolvers].
  *
+ * @receiver The string in _MiniMessage_ format to be deserialized.
  * @param tagResolvers The array of tag resolvers to be used during deserialization.
  * @return The deserialized component.
  * @see net.kyori.adventure.text.minimessage.MiniMessage.deserialize
  */
 fun String.text(vararg tagResolvers: TagResolver) = miniMessage.deserialize(this, *tagResolvers)
+
+/**
+ * Returns this Component if it is not null, otherwise returns an empty Component.
+ *
+ * @receiver the Component being checked for null
+ * @return the original Component if it is not null, a new empty Component otherwise
+ */
+fun Component?.orEmpty() = this ?: Component.empty()
