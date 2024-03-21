4015bdef28079dc9af091ede8b542adcfcbef500
Tozy Fullbuster <tozyfullbuster@gmail.com>
2024-03-21 20:25:43 +0700

    feat(commands): shortcut command registration function

---
+++ core/src/io/github/tozydev/patek/commands/CommandApiCommands.kt
@@ -0,0 +1,13 @@
+@file:Suppress("unused")
+
+package io.github.tozydev.patek.commands
+
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.commandAPICommand
+
+/**
+ * Creates and registers a new command with the given [name] and [block] using the CommandAPI.
+ *
+ * @see commandAPICommand
+ */
+fun command(name: String, block: CommandAPICommand.() -> Unit) = commandAPICommand(name, block)
