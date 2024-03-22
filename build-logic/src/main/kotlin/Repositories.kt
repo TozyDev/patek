70b4d1138a1340017eb3d0ca7f10235e65e1ff78
Tozy Fullbuster <tozyfullbuster@gmail.com>
2024-03-22 17:47:55 +0700

    build: fix wrong repo url

--- build-logic/src/main/kotlin/Repositories.kt
+++ build-logic/src/main/kotlin/Repositories.kt
@@ -7,19 +7,22 @@ const val TOZYDEV_RELEASES_REPO = "https://maven.nguyenthanhtan.id.vn/releases"
 const val TOZYDEV_SNAPSHOT_REPO = "https://maven.nguyenthanhtan.id.vn/snapshots"
 private const val SNAPSHOT_SUFFIX = "-SNAPSHOT"
 
-fun RepositoryHandler.tozydev(project: Project) = maven {
-    name = TOZYDEV_REPO
-    url = if (project.version.toString().endsWith(SNAPSHOT_SUFFIX)) {
-        project.uri(TOZYDEV_RELEASES_REPO)
-    } else {
-        project.uri(TOZYDEV_SNAPSHOT_REPO)
+fun RepositoryHandler.tozydev(project: Project) =
+    maven {
+        name = TOZYDEV_REPO
+        url =
+            if (project.version.toString().endsWith(SNAPSHOT_SUFFIX)) {
+                project.uri(TOZYDEV_SNAPSHOT_REPO)
+            } else {
+                project.uri(TOZYDEV_RELEASES_REPO)
+            }
+        credentials {
+            username = project.environmentOrProperty("MAVEN_USERNAME", "maven.username").orNull
+            password = project.environmentOrProperty("MAVEN_TOKEN", "maven.token").orNull
+        }
     }
-    credentials {
-        username = project.environmentOrProperty("MAVEN_USERNAME", "maven.username").orNull
-        password = project.environmentOrProperty("MAVEN_TOKEN", "maven.token").orNull
-    }
-}
 
-fun RepositoryHandler.codeMC() = maven("https://repo.codemc.org/repository/maven-public/") {
-    name = "CodeMC"
-}
+fun RepositoryHandler.codeMC() =
+    maven("https://repo.codemc.org/repository/maven-public/") {
+        name = "CodeMC"
+    }
