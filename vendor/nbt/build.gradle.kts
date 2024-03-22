ee97b12b3abbc84805d7717d64ed656ba576b413
Tozy Fullbuster <tozyfullbuster@gmail.com>
2024-03-22 17:37:30 +0700

    feat: nbt module
    - This module is the shaded of item-nbt-api by @tr7zw

---
+++ vendor/nbt/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.shadow)
+    id("patek.publishing-conventions")
+}
+
+val itemNbtApi: Configuration by configurations.creating {
+    isCanBeResolved = true
+    isCanBeConsumed = false
+}
+
+repositories {
+    codeMC()
+}
+
+dependencies {
+    itemNbtApi(libs.item.nbt.api)
+}
+
+tasks {
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+
+    shadowJar {
+        archiveClassifier = ""
+        configurations = listOf(itemNbtApi)
+        relocate("de.tr7zw.changeme.nbtapi", "io.github.tozydev.patek.nbt")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+publishing {
+    publications.createPublication(components["java"]).configure {
+        pom {
+            name = "Patek NBT"
+            description = "A library for reading and writing NBT data. (Shaded from NBTAPI by tr7zw)"
+            licenses {
+                license {
+                    name = "MIT"
+                    url = "https://github.com/tr7zw/Item-NBT-API/blob/master/LICENSE"
+                }
+            }
+        }
+    }
+}
